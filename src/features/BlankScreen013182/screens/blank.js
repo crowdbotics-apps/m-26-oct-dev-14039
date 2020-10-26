6d63fb9bf8262d638c964a6df8c1a6aae091f824
Crowdbotics <team@crowdbotics.com>
2020-10-26 13:00:25 +0000

    Update components

--- src/features/BlankScreen013182/screens/blank.js
+++ src/features/BlankScreen013182/screens/blank.js
@@ -28,7 +28,9 @@ export default class Blank extends React.Component {
     TextInput_4: "",
     DateTimePicker_5: new Date(""),
     Switch_6: true,
-    DateTimePicker_8: new Date("")
+    DateTimePicker_8: new Date(""),
+    TextInput_9: "",
+    TextInput_10: ""
   }
 
   render = () => (
@@ -41,16 +43,20 @@ export default class Blank extends React.Component {
       />
       <TextInput
         placeholder="Sample text input placeholder"
+        multiline={true}
+        style={styles.TextInput_4}
         value={this.state.TextInput_4}
         onChangeText={nextValue => this.setState({ TextInput_4: nextValue })}
       />
       <DateTimePicker
+        showIcon={false}
         date={this.state.DateTimePicker_5}
         onDateChange={selectedDate =>
           this.setState({ DateTimePicker_5: selectedDate })
         }
       />
       <Switch
+        trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
         value={this.state.Switch_6}
         onValueChange={nextChecked => this.setState({ Switch_6: nextChecked })}
       />
@@ -61,6 +67,17 @@ export default class Blank extends React.Component {
         onDateChange={selectedDate =>
           this.setState({ DateTimePicker_8: selectedDate })
         }
+      />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        multiline={true}
+        value={this.state.TextInput_9}
+        onChangeText={nextValue => this.setState({ TextInput_9: nextValue })}
+      />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_10}
+        onChangeText={nextValue => this.setState({ TextInput_10: nextValue })}
       />
     </View>
   )
@@ -86,5 +103,15 @@ const styles = StyleSheet.create({
   DateTimePicker_5: {},
   Switch_6: {},
   Slider_7: {},
-  DateTimePicker_8: {}
+  DateTimePicker_8: {},
+  View_1: {},
+  Text_2: {},
+  TextInput_3: {},
+  TextInput_4: { height: 100 },
+  DateTimePicker_5: {},
+  Switch_6: {},
+  Slider_7: {},
+  DateTimePicker_8: {},
+  TextInput_9: {},
+  TextInput_10: {}
 })
